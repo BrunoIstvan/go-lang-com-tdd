abe4e170e9eff6576d24cd7591bb55c73b8758e4
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2020-10-05 07:03:01 -0300

    usando context

---
+++ src/learngo/contexto.go
@@ -0,0 +1,40 @@
+package learngo
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+
+	// return func(w http.ResponseWriter, r *http.Request) {
+	// 	ctx := r.Context()
+
+	// 	data := make(chan string, 1)
+
+	// 	go func() {
+	// 		data <- store.Fetch()
+	// 	}()
+
+	// 	select {
+	// 	case d := <-data:
+	// 		fmt.Fprint(w, d)
+	// 	case <-ctx.Done():
+	// 		store.Cancel()
+	// 	}
+	// }
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			return // todo: registre o erro como você quiser
+		}
+		fmt.Fprint(w, data)
+	}
+
+}
