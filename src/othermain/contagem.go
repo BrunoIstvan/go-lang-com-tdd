2d05817c9a076a978ccad8c51b980c9960556ed8
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2020-10-06 07:36:07 -0300

    refactor e brincando de API com server

--- src/othermain/contagem.go
+++ src/othermain/contagem.go
@@ -1,4 +1,5 @@
-package main
+// package main
+package othermain
 
 import (
 	"fmt"
---
+++ src/player_server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+// NewInMemoryPlayerStore initialises an empty player store.
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{
+		map[string]int{},
+		sync.RWMutex{},
+	}
+}
+
+// InMemoryPlayerStore collects data about players in memory.
+type InMemoryPlayerStore struct {
+	store map[string]int
+	// A mutex is used to synchronize read/write access to the map
+	lock sync.RWMutex
+}
+
+// RecordWin will record a player's win.
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	i.store[name]++
+}
+
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
+	return i.store[name]
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	player := r.URL.Path[len("/players/"):]
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
+
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+
+}
+
+func main() {
+
+	server := &PlayerServer{NewInMemoryPlayerStore()}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
